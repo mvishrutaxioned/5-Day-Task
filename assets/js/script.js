ca4d5e4f642d62a0ba3008f25516264d8d80c730
VishrutMargam <vishrutm@axioned.com>
2021-12-07 18:30:12 +0530

    script fetch result function

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,4 +11,14 @@ $(document).ready(function () {
     var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
     var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
+    // fetch results once
+    fetchResults('mumbai');
+
+    // fetch results function
+    async function fetchResults(i) {
+        var url = `https://api.openweathermap.org/data/2.5/weather?q=${i}&appid=4b9ffb45e1734ffac3f5a513794a0f0d`
+        var data = await fetch(url).then(res => res.json()).catch(e => e)
+        displayWeather(data)
+    }
+
 })
