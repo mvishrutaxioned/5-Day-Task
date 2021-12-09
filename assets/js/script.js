17d9174f0c5e7f45965bd06557f81f48c9c612be
VishrutMargam <vishrutm@axioned.com>
2021-12-09 11:53:18 +0530

    script stop scroll on video

--- assets/js/script.js
+++ assets/js/script.js
@@ -100,6 +100,7 @@ $(document).ready(function () {
         $('.video').fadeOut()
         $('video').get(0).pause();
         $('video').get(0).currentTime = 0;
+        $('html, body').css({"overflow":"visible"})
     }
 
     // on toggleBtn click functionality
@@ -122,6 +123,7 @@ $(document).ready(function () {
         $(this).click(e => {
             e.preventDefault()
             $('.video').fadeIn();
+            $('html, body').css({"overflow":"hidden"})
         })
     })
 
